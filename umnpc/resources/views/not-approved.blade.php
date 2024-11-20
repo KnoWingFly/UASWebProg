fbd00944641de295c08cb57596950682c1e67663
KnoWingFly <112180149+KnoWingFly@users.noreply.github.com>
2024-11-20 12:27:17 +0700

    Add Flowbite + fix the buttton in not approval

--- umnpc/resources/views/not-approved.blade.php
+++ umnpc/resources/views/not-approved.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Account Pending Approval</title>
-    @vite('resources/css/app.css') <!-- Ensure your Tailwind CSS is correctly linked -->
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="bg-white shadow-lg rounded-lg max-w-md mx-auto p-6 text-center">
@@ -12,10 +12,13 @@
         <p class="text-gray-600 mt-2">
             Your account is currently under review by an admin. You will be notified once your account is approved.
         </p>
-        <a href="/logout" class="mt-6 inline-block bg-blue-600 text-white font-medium py-2 px-4 rounded-lg hover:bg-blue-700">
-            Logout
-        </a>
-        <a href="/dashboard" class="mt-4 inline-block bg-indigo-600 text-white font-medium py-2 px-4 rounded-lg hover:bg-indigo-700"></a>
+
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm 
+        px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onclick="window.location.href='/'">Back</button>
+
+        <!-- <a href="/welcome" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm  -->
+        <!-- px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"></a> -->
     </div>
+    <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
