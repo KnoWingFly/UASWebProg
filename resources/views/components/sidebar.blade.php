930427f77abc43c59aba20263d6ab6746aeebe8a
davee32145 <168982808+davee32145@users.noreply.github.com>
2024-12-11 01:19:46 +0700

    Update sidebar.blade.php
    adakan tab baru yaitu manage events

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -42,7 +42,17 @@
                     Manage Users
                 </a>
             </li>
-
+            <li>
+                <a href="{{ route('admin.events.index') }}"
+                    class="flex items-center p-2 text-sm font-medium text-gray-300 rounded-lg hover:bg-gray-700">
+                    <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 8v4m0 0v4m0-4h4m-4 0H8"></path>
+                    </svg>
+                    Manage Events
+                </a>
+            </li>
               <!-- Activity history -->
             <li>
                 <a href="{{ route('admin.activity-history.index') }}"
